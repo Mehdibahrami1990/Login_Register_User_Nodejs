d9fe4a8097f59c33e93696e1fc846b9161abd96e
mehdi <tbmgroup20@gmail.com>
2025-10-27 10:55:46 +0330

    feat: Create user Schema model

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,14 @@
-console.log("mehdi")
+const mongoose = require("mongoose");
+const timeStamp = require("mongoose-timestamp");
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  isadmin: { type: Boolean, default: false },
+});
+
+userSchema.plugin(timeStamp);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
